62c52b9db96fc77408a9b2ffe4aa51d4c8630edc
Svyatoslav Kryukov <me@skryukov.dev>
2024-06-23 14:43:05 +0300

    docs: Show React tab on first visit

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import { setupFrameworksTabs } from './frameworksTabs'
 import './style.css'
 
 export default {
@@ -14,5 +15,8 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
       enhanceAppWithTabs(app)
+  },
+  setup() {
+    setupFrameworksTabs()
   }
 } satisfies Theme
